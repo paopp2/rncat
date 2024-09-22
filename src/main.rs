bc5e9947f310d050a057af2d8c641226e6575dde
Paolo Pepito <npaolopepito@gmail.com>
2024-09-22 16:11:48 +0800

    Implement client sending text over network
    - Delimited by carriage return (on "enter"` pressed)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use clap::{Parser, Subcommand};
 use defaults::*;
-use std::net::IpAddr;
+use std::{
+    io::{self, stdin, BufWriter, Write},
+    net::{IpAddr, TcpStream},
+};
 
 mod defaults {
     pub const DEFAULT_IP_ADDR: &str = "127.0.0.1";
@@ -29,5 +32,29 @@ enum OperationMode {
 
 fn main() {
     let args = Args::parse();
-    println!("{:?}", args);
+
+    match args.mode {
+        OperationMode::Client => match start_client(args.address, args.port) {
+            Ok(_) => println!("Client started"),
+            Err(_) => println!("Failed to start client"),
+        },
+        OperationMode::Server => todo!("Server mode"),
+    }
+}
+
+fn start_client(addr: IpAddr, port: u16) -> io::Result<()> {
+    let address = format!("{}:{}", addr, port);
+    let stream = TcpStream::connect(&address)?;
+    let mut writer = BufWriter::new(stream);
+
+    loop {
+        let outgoing = {
+            let mut input = String::new();
+            stdin().read_line(&mut input)?;
+            input.as_bytes().to_vec()
+        };
+
+        writer.write(&outgoing)?;
+        writer.flush()?;
+    }
 }
